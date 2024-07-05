5243ba7a9cfedb087037c8c07dca3b41b23a57cd
Vivian <vivlopes95@gmail.com>
2024-07-05 21:17:05 +0100

    timing change to navbar

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar({landingRef,aboutRef,skillsRef,projectsRef,expRef
 
     timeoutRef.current = setTimeout(() => {
       setVisible(false)
-    }, 200000)
+    }, 2000)
 
     return () => {
       if (timeoutRef.current) {
