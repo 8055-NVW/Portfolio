73f218cb2f6a45f26abf6a5856db409c432e1177
Vivian <vivlopes95@gmail.com>
2024-12-12 12:24:19 +0000

    made changes to main.jsx to use chreteHashRouter instead of createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,39 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import Root from './Root.jsx'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// // Style imports
+// import './index.css'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//   }
+// ])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router = {router} />
+//   </React.StrictMode>,
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './Root.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 // Style imports
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: '/',
+    path: '/*',  // Use '/*' to catch all routes
     element: <Root />,
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router = {router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
